808acf4a9f108e96df1fbba7fde1ae715237332c
unknown <gamelovr695@gmail.com>
2018-10-02 12:57:14 +0530

    Fix z2 dt2w node

--- app/src/main/java/com/grarak/kerneladiutor/utils/Constants.java
+++ app/src/main/java/com/grarak/kerneladiutor/utils/Constants.java
@@ -812,11 +812,11 @@ public interface Constants {
     String LGE_TOUCH_DT2W = "/sys/devices/virtual/input/lge_touch/dt_wake_enabled";
     String LGE_TOUCH_CORE_DT2W = "/sys/module/lge_touch_core/parameters/doubletap_to_wake";
     String LGE_TOUCH_GESTURE = "/sys/devices/virtual/input/lge_touch/touch_gesture";
-    String DT2W = "/sys/android_touch/doubletap2wake";
+    String DT2W = "/sys/devices/virtual/touch/tp_dev/gesture_on";
     String DT2W_2 = "/sys/android_touch2/doubletap2wake";
     String TOUCH_PANEL_DT2W = "/proc/touchpanel/double_tap_enable";
     String DT2W_WAKEUP_GESTURE = "/sys/devices/virtual/input/input1/wakeup_gesture";
-    String DT2W_ENABLE = "/sys/devices/virtual/touch/tp_dev/gesture_on";
+    String DT2W_ENABLE = "/sys/devices/virtual/touch/tp_dev/gesture_on2";
     String DT2W_WAKE_GESTURE = "/sys/devices/platform/spi-tegra114.2/spi_master/spi2/spi2.0/input/input0/wake_gesture";
     String DT2W_WAKE_GESTURE_2 = "/sys/devices/soc.0/f9924000.i2c/i2c-2/2-0070/input/input0/wake_gesture";
 
